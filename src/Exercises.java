23558b966031b741d0e2cb9245f68752af3c7311
Joseph Wang <jwang@ucvts.tec.nj.us>
2020-02-03 08:37:23 -0500

    Finished middle

--- src/Exercises.java
+++ src/Exercises.java
@@ -58,14 +58,36 @@ public class Exercises {
 	
 	public double biggest(ArrayList<Double> numbers) {
 		// write your code here
-		
-		return -1;		// default return value to ensure compilation
+		if(numbers == null) {
+			return -1;
+		} else if (numbers.size() < 3 || numbers.size() % 2 == 0) {
+			return -1;
+		}
+		double first = numbers.get(0);
+		double middle = numbers.get((int) Math.floor(numbers.size() / 2));
+		double last = numbers.get(numbers.size() -1);
+		double maximum = (first > middle && first > last) ? first :
+		(middle > last) ? middle :
+		last;
+		return maximum;		// default return value to ensure compilation
 	}
 	
 	public ArrayList<String> middle(ArrayList<String> values) {
 		// write your code here
-		
-		return null;	// default return value to ensure compilation
+		ArrayList<String> empty = new ArrayList<String>();
+		if(values == null) {
+			return empty;
+		} else if (values.size() < 3 || values.size() % 2 == 0) {
+			return empty;
+		}
+		String first = values.get((int) Math.floor(values.size() / 2) - 1);
+		String middle = values.get((int) Math.floor(values.size() / 2));
+		String last = values.get((int) Math.floor(values.size() / 2) + 1);
+		ArrayList<String> returnArrayList = new ArrayList<String>();
+		returnArrayList.add(first);
+		returnArrayList.add(middle);
+		returnArrayList.add(last);
+		return returnArrayList;	// default return value to ensure compilation
 	}
 
 	public boolean increasing(ArrayList<Integer> numbers) {
